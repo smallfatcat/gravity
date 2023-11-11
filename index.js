a4e2b91b7012896ed4cf666d331757ae78b8b512
David Imrie <david.imrie@gmail.com>
2023-11-11 22:23:16 +0000

    format

--- index.js
+++ index.js
@@ -14,37 +14,37 @@ let timeFactor = 10;
 let numberOfRocks = 15000;
 let gravityConst = 6.67e-11;
 
-let planarVelFactor = 10;
+let planarVelFactor = 15;
 let outOfPlaneVelFactor = 1;
 let maxMassVariance = 30;
-let baseRadius = 0.5;
-let baseMass = 1e8;
+let baseRadius = 0.8;
+let baseMass = 1e7;
 
 let simStart = Date.now();
 
 let rocks = [];
-for(let i = 0; i < numberOfRocks; i++){
+for (let i = 0; i < numberOfRocks; i++) {
     let massVariance = getRandomInt(1, maxMassVariance);
     let rock = {
-        px: getRandomInt(border, maxwidth-border),
-        py: getRandomInt(border, maxheight-border),
+        px: getRandomInt(border, maxwidth - border),
+        py: getRandomInt(border, maxheight - border),
         pz: getRandomInt(0, maxdepth),
-        r: baseRadius * (massVariance**(1/3)),
+        r: baseRadius * (massVariance ** (1 / 3)),
         m: baseMass * massVariance,
         // vx: 0,
         // vy: 0,
         // vz: 0,
-        vx: ((Math.random()*planarVelFactor)),
-        vy: ((Math.random()*planarVelFactor)),
-        vz: ((Math.random()*outOfPlaneVelFactor*2)-outOfPlaneVelFactor),
+        vx: ((Math.random() * planarVelFactor)),
+        vy: ((Math.random() * planarVelFactor)),
+        vz: ((Math.random() * outOfPlaneVelFactor * 2) - outOfPlaneVelFactor),
         ax: 0.0,
         ay: 0.0,
         az: 0.0
     };
-    if(rock.px<(maxwidth/2)){
+    if (rock.px < (maxwidth / 2)) {
         rock.vy *= -1;
     }
-    if(rock.py>(maxheight/2)){
+    if (rock.py > (maxheight / 2)) {
         rock.vx *= -1;
     }
     rocks.push(rock)
@@ -55,9 +55,9 @@ rocks[0].r = 10;
 rocks[0].vx = 0;
 rocks[0].vy = 0;
 rocks[0].vz = 0;
-rocks[0].px = maxwidth/2;
-rocks[0].py = maxheight/2;
-rocks[0].pz = 0+(maxdepth/2);
+rocks[0].px = maxwidth / 2;
+rocks[0].py = maxheight / 2;
+rocks[0].pz = 0 + (maxdepth / 2);
 
 function draw() {
     frame++;
@@ -67,8 +67,8 @@ function draw() {
     if (canvasLeft.getContext) {
         const ctxLeft = canvasLeft.getContext("2d");
         ctxLeft.clearRect(0, 0, canvasLeft.width, canvasLeft.height);
-        ctxLeft.translate(-rocks[0].px + maxwidth/2, -rocks[0].py + maxheight/2);
-        for(let rock of rocks){
+        ctxLeft.translate(-rocks[0].px + maxwidth / 2, -rocks[0].py + maxheight / 2);
+        for (let rock of rocks) {
             ctxLeft.beginPath();
             ctxLeft.arc(rock.px, rock.py, rock.r, 0, Math.PI * 2, true);
             ctxLeft.stroke();
@@ -79,9 +79,9 @@ function draw() {
     if (canvasRight.getContext) {
         const ctxRight = canvasRight.getContext("2d");
         ctxRight.clearRect(0, 0, canvasRight.width, canvasRight.height);
-        ctxRight.translate(-rocks[0].px + maxwidth/2, -rocks[0].pz + maxheight/2);
-        
-        for(let rock of rocks){
+        ctxRight.translate(-rocks[0].px + maxwidth / 2, -rocks[0].pz + maxheight / 2);
+
+        for (let rock of rocks) {
             ctxRight.beginPath();
             ctxRight.arc(rock.px, rock.pz, rock.r, 0, Math.PI * 2, true);
             ctxRight.stroke();
@@ -89,8 +89,8 @@ function draw() {
         ctxRight.setTransform(1, 0, 0, 1, 0, 0);
     }
     let timestring = Date.now() - startTime;
-    let simTime = Math.floor((Date.now() - simStart)/1000);
-    document.getElementById("status").innerHTML = timestring + ":" + rocks.length + ":" + frame+ ":" + simTime;
+    let simTime = Math.floor((Date.now() - simStart) / 1000);
+    document.getElementById("status").innerHTML = timestring + ":" + rocks.length + ":" + frame + ":" + simTime;
 }
 
 function getRandomInt(min, max) {
@@ -100,21 +100,21 @@ function getRandomInt(min, max) {
 }
 
 function updateRocks() {
-    for(let i = 0; i < numberOfRocks; i++){
+    for (let i = 0; i < numberOfRocks; i++) {
         rocks[i].ax = 0.0;
         rocks[i].ay = 0.0;
         rocks[i].az = 0.0;
     }
 
-    for(let i = 0; i < numberOfRocks; i++){
-        for(let j = i+1; j < numberOfRocks; j++){
+    for (let i = 0; i < numberOfRocks; i++) {
+        for (let j = i + 1; j < numberOfRocks; j++) {
             let dist = getDistance(rocks[i], rocks[j]);
-            let vecToRockx = (rocks[j].px - rocks[i].px)/dist;
-            let vecToRocky = (rocks[j].py - rocks[i].py)/dist;
-            let vecToRockz = (rocks[j].pz - rocks[i].pz)/dist;
-            let force = gravityConst * rocks[i].m * rocks[j].m / (dist*dist);
-            let accela = force/rocks[i].m;
-            let accelb = force/rocks[j].m;
+            let vecToRockx = (rocks[j].px - rocks[i].px) / dist;
+            let vecToRocky = (rocks[j].py - rocks[i].py) / dist;
+            let vecToRockz = (rocks[j].pz - rocks[i].pz) / dist;
+            let force = gravityConst * rocks[i].m * rocks[j].m / (dist * dist);
+            let accela = force / rocks[i].m;
+            let accelb = force / rocks[j].m;
             rocks[i].ax += vecToRockx * accela;
             rocks[i].ay += vecToRocky * accela;
             rocks[i].az += vecToRockz * accela;
@@ -129,58 +129,51 @@ function updateRocks() {
     let oldIndexOfRocks = [];
     let newNumberOfRocks = numberOfRocks;
     let skipFlag = false;
-    for(let i = 0; i < numberOfRocks; i++){
-        for(let j = i+1; j < numberOfRocks; j++){
+    for (let i = 0; i < numberOfRocks; i++) {
+        for (let j = i + 1; j < numberOfRocks; j++) {
             let dist = getDistance(rocks[i], rocks[j]);
-            if(dist <= (rocks[i].r + rocks[j].r)){
+            if (dist <= (rocks[i].r + rocks[j].r)) {
                 rocks[i] = combineRocks(rocks[i], rocks[j]);
-                //rocks.splice(i,1);
-                rocks.splice(j,1);
-                // i = 0;
+                rocks.splice(j, 1);
                 j--;
                 numberOfRocks--;
-                //skipFlag = true;
-                //break;
             }
         }
-        // if(skipFlag){
-        //     continue;
-        // }
     }
-   
+
     // update physics
-    for(let rock of rocks){
+    for (let rock of rocks) {
         rock.vx += rock.ax;
         rock.vy += rock.ay;
         rock.vz += rock.az;
-        rock.px += rock.vx/timeFactor;
-        rock.py += rock.vy/timeFactor;
-        rock.pz += rock.vz/timeFactor;
+        rock.px += rock.vx / timeFactor;
+        rock.py += rock.vy / timeFactor;
+        rock.pz += rock.vz / timeFactor;
     }
 }
 
 function getDistance(a, b) {
-    let dist = Math.sqrt((a.px-b.px)*(a.px-b.px)+(a.py-b.py)*(a.py-b.py)+(a.pz-b.pz)*(a.pz-b.pz));
-    if (dist < (a.r + b.r)/1){
-        dist = (a.r + b.r)/1;
-    } 
+    let dist = Math.sqrt((a.px - b.px) * (a.px - b.px) + (a.py - b.py) * (a.py - b.py) + (a.pz - b.pz) * (a.pz - b.pz));
+    if (dist < (a.r + b.r) / 1) {
+        dist = (a.r + b.r) / 1;
+    }
     return dist;
 }
 
 function combineRocks(a, b) {
     let newm = a.m + b.m;
-    let volumea = 4*Math.PI*(a.r**3)/3; 
-    let volumeb = 4*Math.PI*(b.r**3)/3; 
+    let volumea = 4 * Math.PI * (a.r ** 3) / 3;
+    let volumeb = 4 * Math.PI * (b.r ** 3) / 3;
     let newVolume = volumea + volumeb;
-    let newr = (newVolume*3/(4*Math.PI))**(1/3);
+    let newr = (newVolume * 3 / (4 * Math.PI)) ** (1 / 3);
 
-    let newx = (a.px * a.m + b.px * b.m)/((a.m + b.m));
-    let newy = (a.py * a.m + b.py * b.m)/((a.m + b.m));
-    let newz = (a.pz * a.m + b.pz * b.m)/((a.m + b.m));
+    let newx = (a.px * a.m + b.px * b.m) / ((a.m + b.m));
+    let newy = (a.py * a.m + b.py * b.m) / ((a.m + b.m));
+    let newz = (a.pz * a.m + b.pz * b.m) / ((a.m + b.m));
 
-    let newvx = (a.vx * a.m + b.vx * b.m)/newm;
-    let newvy = (a.vy * a.m + b.vy * b.m)/newm;
-    let newvz = (a.vz * a.m + b.vz * b.m)/newm;
+    let newvx = (a.vx * a.m + b.vx * b.m) / newm;
+    let newvy = (a.vy * a.m + b.vy * b.m) / newm;
+    let newvz = (a.vz * a.m + b.vz * b.m) / newm;
 
     let rock = {
         px: newx,
@@ -197,5 +190,4 @@ function combineRocks(a, b) {
     }
 
     return rock;
-
 }
