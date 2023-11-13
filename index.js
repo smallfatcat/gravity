405dc819da321b4cd802b1f245d363bfd1d62055
David Imrie <david.imrie@gmail.com>
2023-11-13 01:31:06 +0000

    first graphics update

--- index.js
+++ index.js
@@ -8,49 +8,36 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("mass").value = initMass.toExponential();
     document.getElementById("vel").value = initVelocityXY;
     document.getElementById("radius").value = initRadius;
-
+    document.getElementById("solarradius").value = initSolarRadius;
+    document.getElementById("solarmass").value = initSolarMass.toExponential();
 
     setInterval(draw, 10);
 });
 
-function scaleUp() {
-    canvasScale += 0.1;
-}
+const MATERIAL_STAR = 0;
+const MATERIAL_SOLID = 1;
+const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item);
 
-function scaleDn() {
-    canvasScale -= 0.1;
-}
+let canvasWidth     = 800;
+let canvasHeight    = 800;
+let canvasDepth     = 50;
+let canvasBorder    = 100;
+let canvasScale     = 1.0;
 
+let timeFactor      = 10;
+let gravityConst    = 6.67e-11;
 
-function pauseClick() {
-    if(paused){
-        paused = false;
-        document.getElementById("pause").innerHTML = "Pause";
-    }
-    else{
-        paused = true;
-        document.getElementById("pause").innerHTML = "Play";
-    } 
-}
+let initVelocityXY  = 4;
+let initVelocityZ   = 1;
+let initMassVar     = 30;
+let initRadius      = 1.0;
+let initMass        = 1e7;
+let initSolarMass   = 1e14;
+let initSolarRadius = 10;
 
+let numberOfRocks   = 1500;
+let frameCounter    = 0;
 
-let canvasWidth = 800;
-let canvasHeight = 800;
-let canvasDepth = 50;
-let canvasBorder = 100;
-let canvasScale = 1.0;
-
-let timeFactor = 10;
-let gravityConst = 6.67e-11;
-
-let initVelocityXY = 4;
-let initVelocityZ = 1;
-let initMassVariance = 30;
-let initRadius = 1.0;
-let initMass = 1e7;
-
-let numberOfRocks = 1500;
-let frameCounter = 0;
 let frameTime;
 let simTime;
 let simStart = Date.now();
@@ -66,11 +53,13 @@ function reset() {
     numberOfRocks = document.getElementById("startn").value;;
     frameCounter = 0;
     simStart = Date.now();
-    
-    defaultSeed = document.getElementById("seed").value;
-    initRadius = document.getElementById("radius").value;
-    initMass = document.getElementById("mass").value;
-    initVelocityXY = document.getElementById("vel").value;
+
+    defaultSeed     = Number(document.getElementById("seed").value);
+    initRadius      = Number(document.getElementById("radius").value);
+    initMass        = Number(document.getElementById("mass").value);
+    initVelocityXY  = Number(document.getElementById("vel").value);
+    initSolarMass   = Number(document.getElementById("solarmass").value);
+    initSolarRadius = Number(document.getElementById("solarradius").value);
 
     rocks = initRocks(defaultSeed);
     paused = false;
@@ -81,91 +70,96 @@ function initRocks(rockSeed) {
     let seed = rockSeed ^ 0xDEADBEEF; // 32-bit seed with optional XOR value
     rand = sfc32(0x9E3779B9, 0x243F6A88, 0xB7E15162, seed);
     for (let i = 0; i < 15; i++) rand();
-    
+
     let rocks = [];
     for (let i = 0; i < numberOfRocks; i++) {
-        let massVariance = getRandomInt(1, initMassVariance);
+        let massVariance = getRandomInt(1, initMassVar);
+        let randomColor = "rgb(" + getRandomInt(128, 255) + "," + getRandomInt(128, 255) + "," + getRandomInt(128, 255) + ")";
         let rock = {
             px: getRandomInt(canvasBorder, canvasWidth - canvasBorder),
             py: getRandomInt(canvasBorder, canvasHeight - canvasBorder),
             pz: getRandomInt(0, canvasDepth),
-            r:  initRadius * (massVariance ** (1 / 3)),
-            m:  initMass * massVariance,
-            vx: ((rand() * initVelocityXY)),
-            vy: ((rand() * initVelocityXY)),
+            r: initRadius * (massVariance ** (1 / 3)),
+            m: initMass * massVariance,
+            vx: 0.0,
+            vy: 0.0,
             vz: ((rand() * initVelocityZ * 2) - initVelocityZ),
             ax: 0.0,
             ay: 0.0,
-            az: 0.0
+            az: 0.0,
+            material: MATERIAL_SOLID,
+            materialColor: randomColor
         };
-        if (rock.px < (canvasWidth / 2)) {
-            rock.vy *= -1;
-        }
-        if (rock.py > (canvasHeight / 2)) {
-            rock.vx *= -1;
-        }
+        // if (rock.px < (canvasWidth / 2)) {
+        //     rock.vy *= -1;
+        // }
+        // if (rock.py > (canvasHeight / 2)) {
+        //     rock.vx *= -1;
+        // }
         rocks.push(rock)
     }
 
-    rocks[0].m = 1e14;
-    rocks[0].r = 10;
+    rocks[0].m = initSolarMass;
+    rocks[0].r = initSolarRadius;
     rocks[0].vx = 0;
     rocks[0].vy = 0;
     rocks[0].vz = 0;
     rocks[0].px = canvasWidth / 2;
     rocks[0].py = canvasHeight / 2;
     rocks[0].pz = 0 + (canvasDepth / 2);
+    rocks[0].material = MATERIAL_STAR;
+    rocks[0].materialColor = 'rgb(255,255,0)';
+
+    for(let i = 1; i < numberOfRocks; i++){
+        setVelocityVector(rocks[0], rocks[i], (rand() * initVelocityXY) )
+    }
+
     return rocks;
 }
 
 function draw() {
     frameTime = Date.now();
-    if(!paused){
+    if (!paused) {
         frameCounter++;
         simTime = Math.floor((Date.now() - simStart) / 1000);
         updateRocks();
         timestring = Date.now() - frameTime;
     }
-    const canvasLeft = document.getElementById("canvasLeft");
-    if (canvasLeft.getContext) {
-        const ctxLeft = canvasLeft.getContext("2d");
-        ctxLeft.clearRect(0, 0, canvasLeft.width, canvasLeft.height);
-        ctxLeft.scale(canvasScale, canvasScale);
-        ctxLeft.lineWidth = 1 / canvasScale;
-        ctxLeft.translate(-rocks[0].px + canvasWidth / 2 / canvasScale, -rocks[0].py + canvasHeight / 2 / canvasScale);
-        for (let rock of rocks) {
-            ctxLeft.beginPath();
-            ctxLeft.arc(rock.px, rock.py, rock.r, 0, Math.PI * 2, true);
-            ctxLeft.stroke();
-        }
-        ctxLeft.setTransform(1, 0, 0, 1, 0, 0);
-    }
-    const canvasRight = document.getElementById("canvasRight");
-    if (canvasRight.getContext) {
-        const ctxRight = canvasRight.getContext("2d");
-        ctxRight.clearRect(0, 0, canvasRight.width, canvasRight.height);
-        ctxRight.scale(canvasScale, canvasScale);
-        ctxRight.lineWidth = 1 / canvasScale;
-        ctxRight.translate(-rocks[0].px + canvasWidth / 2 / canvasScale, -rocks[0].pz + canvasHeight / 2 / canvasScale);
-        for (let rock of rocks) {
-            ctxRight.beginPath();
-            ctxRight.arc(rock.px, rock.pz, rock.r, 0, Math.PI * 2, true);
-            ctxRight.stroke();
-        }
-        ctxRight.setTransform(1, 0, 0, 1, 0, 0);
-    }
-    
-    document.getElementById("status").innerHTML = "Frame Time:" +timestring
+
+    let rocksSortedZ = clone(rocks);
+    rocksSortedZ.sort((a, b) => a.pz - b.pz);
+    let rocksSortedY = clone(rocks);
+    rocksSortedY.sort((a, b) => a.py - b.py);
+
+    drawCanvas("canvasLeft", false, rocksSortedZ);
+    drawCanvas("canvasRight", true, rocksSortedY);
+
+    document.getElementById("status").innerHTML = "Frame Time:" + timestring
         + " N:" + rocks.length
         + " Frame:" + frameCounter
         + " Time:" + simTime;
-    document.getElementById("scalelabel").innerHTML = (canvasScale*100).toFixed(0) + "%";
+    document.getElementById("scalelabel").innerHTML = (canvasScale * 100).toFixed(0) + "%";
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(rand() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+function drawCanvas(canvasId, topView, sortedRocks) {
+    const canvas = document.getElementById(canvasId);
+    if (canvasLeft.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.scale(canvasScale, canvasScale);
+        ctx.lineWidth = 1 / canvasScale;
+        ctx.translate(-rocks[0].px + canvasWidth / 2 / canvasScale,
+            -(topView ? rocks[0].pz : rocks[0].py) + canvasHeight / 2 / canvasScale
+        );
+        for (let rock of sortedRocks) {
+            ctx.beginPath();
+            ctx.arc(rock.px, topView ? rock.pz : rock.py, rock.r, 0, Math.PI * 2, true);
+            ctx.fillStyle = rock.materialColor;
+            ctx.fill();
+            ctx.stroke();
+        }
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
 }
 
 function updateRocks() {
@@ -219,6 +213,71 @@ function updateRocks() {
     }
 }
 
+function combineRocks(a, b) {
+    let newm = a.m + b.m;
+    let volumea = 4 * Math.PI * (a.r ** 3) / 3;
+    let volumeb = 4 * Math.PI * (b.r ** 3) / 3;
+    let newVolume = volumea + volumeb;
+    let newr = a.material != MATERIAL_STAR ? (newVolume * 3 / (4 * Math.PI)) ** (1 / 3) : a.r;
+
+    let newx = (a.px * a.m + b.px * b.m) / ((a.m + b.m));
+    let newy = (a.py * a.m + b.py * b.m) / ((a.m + b.m));
+    let newz = (a.pz * a.m + b.pz * b.m) / ((a.m + b.m));
+
+    let newvx = (a.vx * a.m + b.vx * b.m) / newm;
+    let newvy = (a.vy * a.m + b.vy * b.m) / newm;
+    let newvz = (a.vz * a.m + b.vz * b.m) / newm;
+
+    let newmaterial = a.material;
+
+    let rock = {
+        px: newx,
+        py: newy,
+        pz: newz,
+        vx: newvx,
+        vy: newvy,
+        vz: newvz,
+        ax: 0,
+        ay: 0,
+        az: 0,
+        m: newm,
+        r: newr,
+        material: newmaterial,
+        materialColor: a.materialColor
+    }
+
+    return rock;
+}
+
+function setVelocityVector(to, from, velocity) {
+    let ox = to.px - from.px;
+    let oy = to.py - from.py;
+    let magO = Math.sqrt(ox*ox+oy*oy);
+    ox = ox / magO;
+    oy = oy / magO;
+    from.vx = oy*velocity;
+    from.vy = -ox*velocity;
+    return from;
+}
+
+function getRadius(density, mass) {
+    // Earth 5000kg/m3
+    // Sun   1400kg/m3
+    let volume = mass/density;
+    let radius = getSphereRadius(volume);
+    return radius;
+}
+
+function getSphereVolume(radius) {
+    let volume = 4/3*Math.PI*(radius**3);
+    return volume;
+}
+
+function getSphereRadius(volume) {
+    let radius = (3*volume/4*Math.PI)**(1/3);
+    return radius;
+}
+
 function getDistance(a, b) {
     let dist = Math.sqrt((a.px - b.px) * (a.px - b.px) + (a.py - b.py) * (a.py - b.py) + (a.pz - b.pz) * (a.pz - b.pz));
     if (dist < (a.r + b.r)) {
@@ -235,36 +294,10 @@ function getDistance2(a, b) {
     return dist2;
 }
 
-function combineRocks(a, b) {
-    let newm = a.m + b.m;
-    let volumea = 4 * Math.PI * (a.r ** 3) / 3;
-    let volumeb = 4 * Math.PI * (b.r ** 3) / 3;
-    let newVolume = volumea + volumeb;
-    let newr = (newVolume * 3 / (4 * Math.PI)) ** (1 / 3);
-
-    let newx = (a.px * a.m + b.px * b.m) / ((a.m + b.m));
-    let newy = (a.py * a.m + b.py * b.m) / ((a.m + b.m));
-    let newz = (a.pz * a.m + b.pz * b.m) / ((a.m + b.m));
-
-    let newvx = (a.vx * a.m + b.vx * b.m) / newm;
-    let newvy = (a.vy * a.m + b.vy * b.m) / newm;
-    let newvz = (a.vz * a.m + b.vz * b.m) / newm;
-
-    let rock = {
-        px: newx,
-        py: newy,
-        pz: newz,
-        vx: newvx,
-        vy: newvy,
-        vz: newvz,
-        ax: 0,
-        ay: 0,
-        az: 0,
-        m: newm,
-        r: newr
-    }
-
-    return rock;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(rand() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
 function sfc32(a, b, c, d) {
@@ -278,5 +311,24 @@ function sfc32(a, b, c, d) {
         t = t + d | 0;
         c = c + t | 0;
         return (t >>> 0) / 4294967296;
+    }
+}
+
+function scaleUp() {
+    canvasScale += 0.1;
+}
+
+function scaleDn() {
+    canvasScale -= 0.1;
+}
+
+function pauseClick() {
+    if (paused) {
+        paused = false;
+        document.getElementById("pause").innerHTML = "Pause";
+    }
+    else {
+        paused = true;
+        document.getElementById("pause").innerHTML = "Play";
     }
 }
